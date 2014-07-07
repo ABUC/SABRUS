24382de4123ead599ea430d82e664b5e6fea91d8
Jakob <deadzac@gmx.net>
2014-07-07 10:32:39 +0200

    Allow all local IPs for app_dev.php, allow all IPs for app.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,9 +9,12 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+$remoteAddress = @$_SERVER['REMOTE_ADDR'];
+$isLocal = strpos($remoteAddress, '192.168.') === 0 || strpos($remoteAddress, '10.') === 0;
+
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '10.8.84.22', '10.8.27.219')) || php_sapi_name() === 'cli-server')
+    || (!$isLocal && !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server'))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
